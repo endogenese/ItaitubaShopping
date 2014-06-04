977406c1c15716b3b174a263353b2f3a75f8664e
fabriciosribeiro <fabricio.ss.ribeiro@gmail.com>
2014-06-04 20:01:23 -0300

    Remodelagem no Menu
    Remodelagem no Menu

--- application/views/vhome.php
+++ application/views/vhome.php
@@ -44,11 +44,12 @@
 						            <span class="icon-bar"></span>
 						        </button>
 								
-								<a class="navbar-brand" href="#">Itaituba Shopping</a>
+								<!-- Esse brand será exibido na versão para phone -->
+								<a class="navbar-brand hidden-lg hidden-md hidden-sm" href="#">Itaituba Shopping</a>
 							</div><!--/.navbar-header -->
 
 							<div class="collapse navbar-collapse">
-						        <ul class="nav navbar-nav">
+						        <ul class="nav navbar-nav ">
 							        <li class="active"><a href="#"><strong>HOME</strong></a></li>
 							        <li><a href="#"><strong>LOJAS</strong></a></li>
 							        <li><a href="#"><strong>CINEMA</strong></a></li>
