1408b6b41bdb6819bbd1d86c683ca50f7ad54dc9
fabriciosribeiro <fabricio.ss.ribeiro@gmail.com>
2014-06-10 19:00:28 -0300

    Add pag Loja
    Add pag Loja

---
+++ lojas/index.php
@@ -0,0 +1,380 @@
+<html lang="pt">
+	<head>
+		<meta charset="utf-8" />
+		<title>Itaituba Shopping</title>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    	<meta name="viewport" content="width=device-width, initial-scale=1">
+	    <meta name="description" content="">
+	    <meta name="author" content="endogenese">
+
+	    <!-- Le styles -->
+	    <link href="/css/bootstrap.css" rel="stylesheet">
+
+		<link href="/css/template.css" rel="stylesheet">
+
+	    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	    <!--[if lt IE 9]>
+	      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+	      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+	    <![endif]-->
+
+	</head>
+
+
+	<body>
+
+		<div class="container">
+
+			<div class="row" id="menu">
+
+				<div class="col-md-12">
+					<nav class="navbar navbar-default" role="navigation">
+
+						<!-- menu justificado -->
+						<div id="menu_justificado" class="container-fluid hidden-xs">
+					        <ul class="nav navbar-nav ">
+						        <li><a href="/"><strong>HOME</strong></a></li>
+						        <li class="active"><a href="lojas/"><strong>LOJAS</strong></a></li>
+						        <li><a href="#"><strong>CINEMA</strong></a></li>
+						        <li><a href="#"><strong>NOVIDADES</strong></a></li>
+						        <li><a href="#"><strong>QUEM SOMOS</strong></a></li>
+					        </ul>
+						</div>
+
+						<!-- menu moblie -->
+						<div class="container-fluid visible-xs">
+
+							<div class="navbar-header">		        
+
+				        		<!-- Menu responsivo-->
+				        		<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+						            <span class="sr-only">Toggle navigation</span>
+						            <span class="icon-bar"></span>
+						        	<span class="icon-bar"></span>
+						            <span class="icon-bar"></span>
+						        </button>
+								
+								<!-- Esse brand será exibido na versão para phone -->
+								<a class="navbar-brand hidden-lg hidden-md hidden-sm" href="#">Itaituba Shopping</a>
+							</div><!--/.navbar-header -->
+
+							<div class="collapse navbar-collapse">
+						        <ul class="nav navbar-nav ">
+							        <li><a href="/"><strong>HOME</strong></a></li>
+							        <li class="active"><a href="lojas/index.php"><strong>LOJAS</strong></a></li>
+							        <li><a href="#"><strong>CINEMA</strong></a></li>
+							        <li><a href="#"><strong>NOVIDADES</strong></a></li>
+							        <li><a href="#"><strong>QUEM SOMOS</strong></a></li>
+						        </ul>
+						    </div><!--/.nav-collapse -->			
+						
+						</div><!---/.container-fluid-->
+					</nav>
+				</div>
+
+			</div><!--/#menu-->
+
+			<div class="row" id="banner_home">
+
+				<div class="col-md-12">
+
+					<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+					  <!-- Indicators -->
+						<ol class="carousel-indicators">
+						    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+						    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+						    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+						 
+						</ol>
+
+						<!-- Wrapper for slides -->
+						<div class="carousel-inner">
+						    <div class="item active">
+						      <img src="http://placehold.it/1138x300" alt="...">
+						      <div class="carousel-caption">
+						        <h3>Slide 1</h3>
+    							<p>Descricação 1</p>
+						      </div>
+						    </div>
+
+						    <div class="item">
+						      <img src="http://placehold.it/1138x300" alt="...">
+						      <div class="carousel-caption">
+						        <h3>Slide 2</h3>
+    							<p>Descricação 2</p>
+						      </div>
+						    </div>
+
+						    <div class="item">
+						      <img src="http://placehold.it/1138x300" alt="...">
+						      <div class="carousel-caption">
+						        <h3>Slide 2</h3>
+    							<p>Descricação 2</p>
+						      </div>
+						    </div>
+						    
+						</div>
+
+						<!-- Controls -->
+						<a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+						    <span class="glyphicon glyphicon-chevron-left"></span>
+						  </a>
+						  <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+						    <span class="glyphicon glyphicon-chevron-right"></span>
+						</a>
+					</div><!--/.carousel-->
+
+
+				</div>
+
+			</div><!--/#BANNER_HOME-->
+
+			<div class="row">
+				<div class="col-md-3">
+					<h2>FILTRO DE LOJAS</h2>
+				</div>
+			</div>
+
+			<div class="row" id="corpo_loja">
+				<div class="col-md-3">
+					<div id="lista_lojas">
+
+						<ul>
+			              <li><a id="loja1-m" href="#"> <img src="http://placehold.it/30x30"/> Loja 1</a></li>
+			              <li><a id="loja2-m" href="#"> <img src="http://placehold.it/30x30"/> Loja 2</a></li>
+			              <li><a id="loja3-m" href="#"> <img src="http://placehold.it/30x30"/> Loja 3</a></li>
+			              <li><a id="loja4-m" href="#"> <img src="http://placehold.it/30x30"/> Loja 4</a></li>
+			              <li><a id="loja5-m" href="#"> <img src="http://placehold.it/30x30"/> Loja 5</a></li>
+			              <li><a id="loja6-m" href="#"> <img src="http://placehold.it/30x30"/> Loja 6</a></li>
+			              <li><a id="loja7-m" href="#"> <img src="http://placehold.it/30x30"/> Loja 7</a></li>
+			              <li><a id="loja8-m" href="#"> <img src="http://placehold.it/30x30"/> Loja 8</a></li>
+			              <li><a id="loja9-m" href="#"> <img src="http://placehold.it/30x30"/> Loja 9</a></li>
+			              <li><a id="loja10-m" href="#"> <img src="http://placehold.it/30x30"/> Loja 10</a></li>
+			            </ul>
+			        
+					</div><!-- /#lojas -->
+				</div>
+
+				<div class="col-md-9">
+					<div id="lojas_container">
+
+						<div id="loja1" class="lojas">
+							<div  id="logo_loja">
+								<img src="http://placehold.it/700x150"/>
+							</div>
+							<h4 style="text-align:center">Loja 1</h4>
+							<p>Lojas com produtos de telefonía, informática, cd´s, dvd´s, livros, revistas, esporte, eletrodomésticos, eletrônicos, brinquedos, bebés, meias e lingerie, relogios.  Preços baixos e ótimas ofertas de celulares, notebooks, TVs LED e 3D, geladeiras, câmeras digitais, tablets, DVDs e mais na Americanas.com. Compre já!</p>
+							<p>Lojas com produtos de telefonía, informática, cd´s, dvd´s, livros, revistas, esporte, eletrodomésticos, eletrônicos, brinquedos, bebés, meias e lingerie, relogios.  Preços baixos e ótimas ofertas de celulares, notebooks, TVs LED e 3D, geladeiras, câmeras digitais, tablets, DVDs e mais na Americanas.com. Compre já!</p>						
+						</div><!-- /.lojas -->
+
+						<div id="loja2" class="lojas">
+							<div  id="logo_loja">
+								<img src="http://placehold.it/700x150"/>
+							</div>
+							<h4 style="text-align:center">Loja 2</h4>
+							<p>Lojas com produtos de telefonía, informática, cd´s, dvd´s, livros, revistas, esporte, eletrodomésticos, eletrônicos, brinquedos, bebés, meias e lingerie, relogios.  Preços baixos e ótimas ofertas de celulares, notebooks, TVs LED e 3D, geladeiras, câmeras digitais, tablets, DVDs e mais na Americanas.com. Compre já!</p>
+							<p>Lojas com produtos de telefonía, informática, cd´s, dvd´s, livros, revistas, esporte, eletrodomésticos, eletrônicos, brinquedos, bebés, meias e lingerie, relogios.  Preços baixos e ótimas ofertas de celulares, notebooks, TVs LED e 3D, geladeiras, câmeras digitais, tablets, DVDs e mais na Americanas.com. Compre já!</p>						
+						</div><!-- /.lojas -->
+
+						<div id="loja3" class="lojas">
+							<div  id="logo_loja">
+								<img src="http://placehold.it/700x150"/>
+							</div>
+							<h4 style="text-align:center">Loja 3</h4>
+							<p>Lojas com produtos de telefonía, informática, cd´s, dvd´s, livros, revistas, esporte, eletrodomésticos, eletrônicos, brinquedos, bebés, meias e lingerie, relogios.  Preços baixos e ótimas ofertas de celulares, notebooks, TVs LED e 3D, geladeiras, câmeras digitais, tablets, DVDs e mais na Americanas.com. Compre já!</p>
+							<p>Lojas com produtos de telefonía, informática, cd´s, dvd´s, livros, revistas, esporte, eletrodomésticos, eletrônicos, brinquedos, bebés, meias e lingerie, relogios.  Preços baixos e ótimas ofertas de celulares, notebooks, TVs LED e 3D, geladeiras, câmeras digitais, tablets, DVDs e mais na Americanas.com. Compre já!</p>						
+						</div><!-- /.lojas -->
+
+						<div id="loja4" class="lojas">
+							<div  id="logo_loja">
+								<img src="http://placehold.it/700x150"/>
+							</div>
+							<h4 style="text-align:center">Loja 4</h4>
+							<p>Lojas com produtos de telefonía, informática, cd´s, dvd´s, livros, revistas, esporte, eletrodomésticos, eletrônicos, brinquedos, bebés, meias e lingerie, relogios.  Preços baixos e ótimas ofertas de celulares, notebooks, TVs LED e 3D, geladeiras, câmeras digitais, tablets, DVDs e mais na Americanas.com. Compre já!</p>
+							<p>Lojas com produtos de telefonía, informática, cd´s, dvd´s, livros, revistas, esporte, eletrodomésticos, eletrônicos, brinquedos, bebés, meias e lingerie, relogios.  Preços baixos e ótimas ofertas de celulares, notebooks, TVs LED e 3D, geladeiras, câmeras digitais, tablets, DVDs e mais na Americanas.com. Compre já!</p>						
+						</div><!-- /.lojas -->
+
+						<div id="loja5" class="lojas">
+							<div  id="logo_loja">
+								<img src="http://placehold.it/700x150"/>
+							</div>
+							<h4 style="text-align:center">Loja 5</h4>
+							<p>Lojas com produtos de telefonía, informática, cd´s, dvd´s, livros, revistas, esporte, eletrodomésticos, eletrônicos, brinquedos, bebés, meias e lingerie, relogios.  Preços baixos e ótimas ofertas de celulares, notebooks, TVs LED e 3D, geladeiras, câmeras digitais, tablets, DVDs e mais na Americanas.com. Compre já!</p>
+							<p>Lojas com produtos de telefonía, informática, cd´s, dvd´s, livros, revistas, esporte, eletrodomésticos, eletrônicos, brinquedos, bebés, meias e lingerie, relogios.  Preços baixos e ótimas ofertas de celulares, notebooks, TVs LED e 3D, geladeiras, câmeras digitais, tablets, DVDs e mais na Americanas.com. Compre já!</p>						
+						</div><!-- /.lojas -->
+
+						<div id="loja6" class="lojas">
+							<div  id="logo_loja">
+								<img src="http://placehold.it/700x150"/>
+							</div>
+							<h4 style="text-align:center">Loja 6</h4>
+							<p>Lojas com produtos de telefonía, informática, cd´s, dvd´s, livros, revistas, esporte, eletrodomésticos, eletrônicos, brinquedos, bebés, meias e lingerie, relogios.  Preços baixos e ótimas ofertas de celulares, notebooks, TVs LED e 3D, geladeiras, câmeras digitais, tablets, DVDs e mais na Americanas.com. Compre já!</p>
+							<p>Lojas com produtos de telefonía, informática, cd´s, dvd´s, livros, revistas, esporte, eletrodomésticos, eletrônicos, brinquedos, bebés, meias e lingerie, relogios.  Preços baixos e ótimas ofertas de celulares, notebooks, TVs LED e 3D, geladeiras, câmeras digitais, tablets, DVDs e mais na Americanas.com. Compre já!</p>						
+						</div><!-- /.lojas -->
+
+						<div id="loja7" class="lojas">
+							<div  id="logo_loja">
+								<img src="http://placehold.it/700x150"/>
+							</div>
+							<h4 style="text-align:center">Loja 7</h4>
+							<p>Lojas com produtos de telefonía, informática, cd´s, dvd´s, livros, revistas, esporte, eletrodomésticos, eletrônicos, brinquedos, bebés, meias e lingerie, relogios.  Preços baixos e ótimas ofertas de celulares, notebooks, TVs LED e 3D, geladeiras, câmeras digitais, tablets, DVDs e mais na Americanas.com. Compre já!</p>
+							<p>Lojas com produtos de telefonía, informática, cd´s, dvd´s, livros, revistas, esporte, eletrodomésticos, eletrônicos, brinquedos, bebés, meias e lingerie, relogios.  Preços baixos e ótimas ofertas de celulares, notebooks, TVs LED e 3D, geladeiras, câmeras digitais, tablets, DVDs e mais na Americanas.com. Compre já!</p>						
+						</div><!-- /.lojas -->
+
+						<div id="loja8" class="lojas">
+							<div  id="logo_loja">
+								<img src="http://placehold.it/700x150"/>
+							</div>
+							<h4 style="text-align:center">Loja 8</h4>
+							<p>Lojas com produtos de telefonía, informática, cd´s, dvd´s, livros, revistas, esporte, eletrodomésticos, eletrônicos, brinquedos, bebés, meias e lingerie, relogios.  Preços baixos e ótimas ofertas de celulares, notebooks, TVs LED e 3D, geladeiras, câmeras digitais, tablets, DVDs e mais na Americanas.com. Compre já!</p>
+							<p>Lojas com produtos de telefonía, informática, cd´s, dvd´s, livros, revistas, esporte, eletrodomésticos, eletrônicos, brinquedos, bebés, meias e lingerie, relogios.  Preços baixos e ótimas ofertas de celulares, notebooks, TVs LED e 3D, geladeiras, câmeras digitais, tablets, DVDs e mais na Americanas.com. Compre já!</p>						
+						</div><!-- /.lojas -->
+
+						<div id="loja9" class="lojas">
+							<div  id="logo_loja">
+								<img src="http://placehold.it/700x150"/>
+							</div>
+							<h4 style="text-align:center">Loja 9</h4>
+							<p>Lojas com produtos de telefonía, informática, cd´s, dvd´s, livros, revistas, esporte, eletrodomésticos, eletrônicos, brinquedos, bebés, meias e lingerie, relogios.  Preços baixos e ótimas ofertas de celulares, notebooks, TVs LED e 3D, geladeiras, câmeras digitais, tablets, DVDs e mais na Americanas.com. Compre já!</p>
+							<p>Lojas com produtos de telefonía, informática, cd´s, dvd´s, livros, revistas, esporte, eletrodomésticos, eletrônicos, brinquedos, bebés, meias e lingerie, relogios.  Preços baixos e ótimas ofertas de celulares, notebooks, TVs LED e 3D, geladeiras, câmeras digitais, tablets, DVDs e mais na Americanas.com. Compre já!</p>						
+						</div><!-- /.lojas -->
+
+						<div id="loja10" class="lojas">
+							<div  id="logo_loja">
+								<img src="http://placehold.it/700x150"/>
+							</div>
+							<h4 style="text-align:center">Loja 10</h4>
+							<p>Lojas com produtos de telefonía, informática, cd´s, dvd´s, livros, revistas, esporte, eletrodomésticos, eletrônicos, brinquedos, bebés, meias e lingerie, relogios.  Preços baixos e ótimas ofertas de celulares, notebooks, TVs LED e 3D, geladeiras, câmeras digitais, tablets, DVDs e mais na Americanas.com. Compre já!</p>
+							<p>Lojas com produtos de telefonía, informática, cd´s, dvd´s, livros, revistas, esporte, eletrodomésticos, eletrônicos, brinquedos, bebés, meias e lingerie, relogios.  Preços baixos e ótimas ofertas de celulares, notebooks, TVs LED e 3D, geladeiras, câmeras digitais, tablets, DVDs e mais na Americanas.com. Compre já!</p>						
+						</div><!-- /.lojas -->
+
+
+					</div><!-- /#lojas_container-->
+				</div>
+
+			</div><!--/#CORPO_LOJA-->
+			
+			<div class="col-md-12">
+				<div class="row" id="rodape">
+
+					<p class="text-center">Siga-nos nas redes sociais</p>
+					
+					<div id="redes_sociais">
+						<img src="/img/facebook.png" alt="Facebook"/>
+						<img src="/img/twitter.png" alt="Twitter"/>
+					</div>
+					
+					<p class="text-center">2014 - Itaituba Shopping - Todos os direitos reservados</p>
+					<p class="text-center"><small>Desenvolvido por <a href="http://wwww.endogense.com.br">Endogênese</a></small></p>
+					<p id="teste"></p>	
+
+				</div><!--/.rodape-->
+
+			</div>
+
+
+		</div> <!--/.CONTAINER-->
+
+		<!-- Arquivos Javascripts
+	    ================================================== -->
+	    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+	    <script src="/js/bootstrap.min.js"></script>
+
+	    <script>
+
+	    	$(document).ready(function(){
+
+	    		$('.lojas').addClass('ocultar_lojas');
+	    		
+	    		// var i = $('.lojas').length
+	    		// i=3;
+	    		
+	    		 $('#loja1-m').click(function(){
+	    			if($('.lojas').hasClass('exibir_loja'))
+	    		 		$('.lojas').removeClass('exibir_loja');
+
+	    		 	$('#loja1').addClass('exibir_loja');	
+	    		 });
+
+	    		 $('#loja2-m').click(function(){
+	    		 	if($('.lojas').hasClass('exibir_loja'))
+	    		 		$('.lojas').removeClass('exibir_loja');
+
+	    		 	$('#loja2').addClass('exibir_loja');	
+	    		 });
+
+	    		 $('#loja3-m').click(function(){
+	    		 	if($('.lojas').hasClass('exibir_loja'))
+	    		 		$('.lojas').removeClass('exibir_loja');
+
+	    		 	$('#loja3').addClass('exibir_loja');	
+	    		 });
+
+	    		 $('#loja4-m').click(function(){
+	    		 	if($('.lojas').hasClass('exibir_loja'))
+	    		 		$('.lojas').removeClass('exibir_loja');
+
+	    		 	$('#loja4').addClass('exibir_loja');	
+	    		 });
+
+	    		 $('#loja5-m').click(function(){
+	    		 	if($('.lojas').hasClass('exibir_loja'))
+	    		 		$('.lojas').removeClass('exibir_loja');
+
+	    		 	$('#loja5').addClass('exibir_loja');	
+	    		 });
+
+	    		 $('#loja6-m').click(function(){
+	    		 	if($('.lojas').hasClass('exibir_loja'))
+	    		 		$('.lojas').removeClass('exibir_loja');
+
+	    		 	$('#loja6').addClass('exibir_loja');	
+	    		 });
+
+	    		 $('#loja7-m').click(function(){
+	    		 	if($('.lojas').hasClass('exibir_loja'))
+	    		 		$('.lojas').removeClass('exibir_loja');
+
+	    		 	$('#loja7').addClass('exibir_loja');	
+	    		 });
+
+	    		 $('#loja8-m').click(function(){
+	    		 	if($('.lojas').hasClass('exibir_loja'))
+	    		 		$('.lojas').removeClass('exibir_loja');
+
+	    		 	$('#loja8').addClass('exibir_loja');	
+	    		 });
+
+	    		 $('#loja9-m').click(function(){
+	    		 	if($('.lojas').hasClass('exibir_loja'))
+	    		 		$('.lojas').removeClass('exibir_loja');
+
+	    		 	$('#loja9').addClass('exibir_loja');	
+	    		 });
+
+	    		 $('#loja10-m').click(function(){
+	    		 	if($('.lojas').hasClass('exibir_loja'))
+	    		 		$('.lojas').removeClass('exibir_loja');
+
+	    		 	$('#loja10').addClass('exibir_loja');	
+	    		 });
+
+	    		// for(var i = 3 ; i<=3; i++ ){
+
+		    	// 	 $('#loja'+i+'-m').click(function(){
+		    	// 	 	if($('.lojas').hasClass('exibir_loja'))
+		    	// 	 		$('.lojas').removeClass('exibir_loja');
+
+		    	// 	 	$('#loja'+i).addClass('exibir_loja');	
+		    	// 	 });
+
+		    	// 	 $('#teste').text("sds"+i);
+	    		// }
+	    	});
+	    </script>
+
+
+	</body>
+
+</html>
